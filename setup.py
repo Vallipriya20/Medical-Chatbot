e583057eb2d768ca04dc7acacab666cb1d6ef3f6
Valli Priya <vallipriya2218@gmail.com>
2025-04-03 14:55:35 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Valli Priya',
+    author_email = 'vallipriya2218@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
